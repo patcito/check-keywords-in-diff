e9d2b1bafdf5834d321fa29c53c6b56d9423089a
Patrick Aljord <patcito@gmail.com>
2021-05-06 22:50:57 +0400

    check diff and send notification to github PR

--- src/inputs.ts
+++ src/inputs.ts
@@ -4,19 +4,6 @@ import * as Inputs from './namespaces/Inputs';
 type GetInput = (name: string, options?: InputOptions | undefined) => string;
 
 export const parseInputs = (getInput: GetInput): Inputs.Args => {
-  const old = getInput('old', {required: true});
-  const newPath = getInput('new', {required: true});
-  const mode = getInput('mode', {required: true}) as Inputs.Mode;
-  const tolerance = getInput('tolerance', {required: true}) as Inputs.Tolerance;
-  const output = getInput('output');
-  if (!Object.values(Inputs.Mode).includes(mode)) {
-    throw new Error(`invalid value for 'mode': '${mode}'`);
-  }
-
-  if (!Object.values(Inputs.Tolerance).includes(tolerance)) {
-    throw new Error(`invalid value for 'tolerance': '${tolerance}'`);
-  }
-
   let notifications;
   const notify_check = getInput('notify_check');
   const notify_issue = getInput('notify_issue');
@@ -32,13 +19,6 @@ export const parseInputs = (getInput: GetInput): Inputs.Args => {
   }
 
   return {
-    old,
-    new: newPath,
-
-    mode,
-    tolerance,
-    output,
-
     notifications,
   };
 };
