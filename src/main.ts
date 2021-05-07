f056cb2e9227e46e9d71527f1b2c2b5dc4b3cdc6
Patrick Aljord <patcito@gmail.com>
2021-05-07 22:54:52 +0400

    don't fail when nothing is found, just don't notify PR. update build

--- src/main.ts
+++ src/main.ts
@@ -11,27 +11,24 @@ async function run(): Promise<void> {
     console.log('inputs', inputs);
     core.debug(`Calculate result`);
     const result = await processDiff(inputs.branch);
-
-    if (inputs.notifications) {
-      core.debug(`Setting up OctoKit`);
-      if (inputs.notifications.check) {
-        core.debug(`Notification: Check Run`);
-      }
-      if (inputs.notifications.issue) {
-        core.debug(`Notification: Issue`);
-        await createComment(result, inputs.notifications.token, inputs.notifications.label);
+    if (result.passed) {
+      if (inputs.notifications) {
+        core.debug(`Setting up OctoKit`);
+        if (inputs.notifications.check) {
+          core.debug(`Notification: Check Run`);
+        }
+        if (inputs.notifications.issue) {
+          core.debug(`Notification: Issue`);
+          await createComment(result, inputs.notifications.token, inputs.notifications.label);
+        }
       }
     }
 
-    core.debug(`Checking tolerance`);
-    if (!result.passed) {
-      core.setFailed(result.summary);
-    }
     core.info(result.summary);
     core.info('===');
 
     core.debug(`Setting outputs`);
-    core.setOutput('passed', result.passed ? 'true' : 'false');
+    core.setOutput('found something', result.passed ? 'true' : 'false');
 
     core.debug(`Done`);
   } catch (error) {
